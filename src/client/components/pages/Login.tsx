24bf12ed3547cbe23fcc8433e4600a8cde110de9
the-real-nox <the-real-nox@outlook.com>
2025-02-22 18:22:26 +0100

    Began working on the frontend pages.

---
+++ src/client/components/pages/Login.tsx
@@ -0,0 +1,9 @@
+import MainLayout from "../MainLayout";
+
+export default function Login() {
+    return (
+        <MainLayout>
+            <h1>Hello</h1>
+        </MainLayout>
+    )
+}
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,12 +1,17 @@
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+import Login from "./components/pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"/>
+        <Route path="/login" element={ <Login /> }/>
+        <Route path="/register"/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
